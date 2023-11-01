2f11b4d90f3c05720b2877e62685afcf403eda13
BishwasKoirala <wasubisu69@gmail.com>
2023-11-01 17:11:28 +0900

    sex

--- app/bookRecords/page.tsx
+++ app/bookRecords/page.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
+import { createClient } from '@supabase/supabase-js'
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient('https://ivoaofipzllrsyzymtha.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml2b2FvZmlwemxscnN5enltdGhhIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5NzYyMjcxOCwiZXhwIjoyMDEzMTk4NzE4fQ.ohmO9rBGH5H31wvD9TmHxzln7g9M5COWTQGpCZE1-QY')
+
 const BookRecordsPage = () => {
   return (
-    <div>BookRecordsPage</div>
+    <div>
+      <h1>BookRecordsPage</h1>
+    </div>
   )
 }
 
