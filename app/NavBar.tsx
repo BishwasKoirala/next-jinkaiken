893bb6df0824392dec907d23d7865e2c8b9482fb
BishwasKoirala <wasubisu69@gmail.com>
2023-11-04 00:39:35 +0900

    comitting

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -8,7 +8,7 @@ const NavBar = () => {
       <Link href="/" className='p-3 mr-1 bg-gray-700'>Home</Link>
       
       <Link href="./rentReturn" className='p-3  mr-1 bg-gray-700'>本貸出／返却登録</Link>
-      <Link href="./bookRecords" className='p-3 mr-1 bg-gray-700'>部内の本の拝借履歴</Link>
+      <Link href="./bookRecords" className='p-3 mr-1 bg-gray-700'>部内の本の貸借履歴</Link>
     </div>
   )
 }
