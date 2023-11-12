a6996c915447eecb933f22965f1a7ad6d35b8cd3
BishwasKoirala <wasubisu69@gmail.com>
2023-11-13 02:56:03 +0900

    commitign

---
+++ app/api/users/[id]/route.tsx
@@ -0,0 +1,35 @@
+import { NextRequest, NextResponse } from "next/server";
+
+interface Props {
+  param : {id : number }
+}
+
+export function GET(
+  request:NextRequest,
+  {params} : { params:{id: number}}) {
+    if (params.id > 10){
+      return NextResponse.json({error: 'user not found'}, {status : 404})
+    } else {
+    return NextResponse.json({id :1 , name : 'mosh'})
+  }
+} 
+
+export async function PUT(
+  request:NextRequest,
+  {params} : { params:{id: number}}) {
+  // validate requset body  
+  const body = await request.json();
+  // if invalid return 404
+  if (!body.name)
+    return NextResponse.json({ error : "Name is required"},{status : 404})
+  // if valid , then fetch with given user id
+  
+  // if doest exist return 404
+  if(params.id > 10 )
+  return NextResponse.json({error: "user not found"},{status : 404})
+  // otherwise, update the userdata and return
+
+  return NextResponse.json({id :1,name :body.name})
+
+}
+
--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -7,3 +7,13 @@ export function GET(request:NextRequest) {
 
   ]);
 }
+
+// あえてrequest を requesrw　にしてます
+export async function POST(request:NextRequest) {
+  const body = await request.json();
+  // validateit , if valit 200, invalid 400
+  if (!body.name){
+    return NextResponse.json({error : 'Name is required'}, { status : 400}) 
+  }
+  return NextResponse.json({id :1 , name : body.name},{status:201});
+}
