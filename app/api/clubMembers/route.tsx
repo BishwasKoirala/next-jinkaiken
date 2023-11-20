c7914d9f7deeaf079488a53b3dea34799d9e0a55
BishwasKoirala <wasubisu69@gmail.com>
2023-11-20 13:08:33 +0900

    wtf is changed??

--- app/api/clubMembers/route.tsx
+++ app/api/clubMembers/route.tsx
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
 import prisma from "@/prisma/client";
 
- export async function GET(request:NextRequest) {
+export async function GET(request:NextRequest) {
   const clubMember = await prisma.clubMember.findMany();
 
   return NextResponse.json(clubMember);
