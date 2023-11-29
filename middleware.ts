975d07af441ac984f327f420705e193cdf05595e
BishwasKoirala <wasubisu69@gmail.com>
2023-11-30 03:30:55 +0900

    changes midddleware code

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,7 @@
-import { NextRequest, NextResponse } from "next/server";
+import middleware from "next-auth/middleware";
 
-export function middleware(request:NextRequest) {
-  return NextResponse.redirect(new URL('/new-page',request.url))
-}
+export default middleware
 
 export const config = {
-  matcher : ['/users:id']
+  matcher : ['/users/:id*']
 }
-
-
-
-// export {default} from "next-auth/middleware";
-
-
-// export const config = {
-//   // *: zero or more params
-//   // + : one or more
-//   // ?: zero or one
-//   matcher : ['/dashboard/:path*']
-// }
