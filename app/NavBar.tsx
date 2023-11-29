160cd9de3a5a5c4922f888038c87bc24dbb568ed
BishwasKoirala <wasubisu69@gmail.com>
2023-11-30 00:25:57 +0900

    do from middleware

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,32 +1,45 @@
-'use client'
-import React from 'react'
-import Link from 'next/link'
-import { useSession } from 'next-auth/react'
+// when using reacthook or react components, use client
+"use client";
+import React from "react";
+import Link from "next/link";
+import { useSession } from "next-auth/react";
 
 const NavBar = () => {
-
-  const {status, data : session} = useSession();
+  const { status, data: session } = useSession();
   return (
-    <div className=' bg-gray-500 p-3 text-lime-200'>
-      
-      <Link href="/" className='p-3 mr-1 bg-gray-700'>Home</Link>
-      
-      <Link href="./rentReturn" className='p-3  mr-1 bg-gray-700'>本の貸/返</Link>
-      <Link href="./bookRecords" className='p-3 mr-1 bg-gray-700'>貸借記録</Link>
+    <div className=" bg-gray-500 p-3 text-lime-200">
+      <Link href="/" className="p-3 mr-1 bg-gray-700">
+        Home
+      </Link>
 
-      <>{ (status === 'loading') && <>Loading...</> }</>
-      <>{status === 'authenticated' && 
-        <>
-        {session.user?.name}
-          <Link href='/api/auth/signout' className='p-3 mr-3 bg-gray-700'>SignOut</Link>
-        </>
-        }
+      <Link href="./rentReturn" className="p-3  mr-1 bg-gray-700">
+        本の貸/返
+      </Link>
+      <Link href="./bookRecords" className="p-3 mr-1 bg-gray-700">
+        貸借記録
+      </Link>
+
+      {/* <>{status === "loading" && <>Loading...</>}</> */}
+      <>
+        {status === "authenticated" && (
+          <>
+            {session.user!.name}
+            <Link href="/api/auth/signout" className="p-3 mr-3 bg-gray-700">
+              SignOut
+            </Link>
+          </>
+        )}
       </>
-      
-      <>{ status === 'unauthenticated' && <Link href="api/auth/signin" className='p-3 mr-1 bg-gray-700'>Login</Link>}
+
+      <>
+        {status === "unauthenticated" && (
+          <Link href="/api/auth/signin" className="p-3 mr-1 bg-gray-700">
+            Login
+          </Link>
+        )}
       </>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,21 @@
-export {default} from "next-auth/middleware";
+import { NextRequest, NextResponse } from "next/server";
 
+export function middleware(request:NextRequest) {
+  return NextResponse.redirect(new URL('/new-page',request.url))
+}
 
 export const config = {
-  // *: zero or more params
-  // + : one or more
-  // ?: zero or one
-  matcher : ['/dashboard/:path*']
+  matcher : ['/users:id']
 }
+
+
+
+// export {default} from "next-auth/middleware";
+
+
+// export const config = {
+//   // *: zero or more params
+//   // + : one or more
+//   // ?: zero or one
+//   matcher : ['/dashboard/:path*']
+// }
