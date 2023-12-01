73dcfa60caf33159f2e0afa4cc51bc858e88e7c0
BishwasKoirala <wasubisu69@gmail.com>
2023-12-01 16:31:28 +0900

    commit

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -71,6 +71,40 @@ export default function Page() {
           onChange={handleChange}
         />
         <br />
+
+        <label htmlFor="gakubu">学部</label>
+        <br />
+        <input
+          type="text"
+          name="gakubu"
+          id="gakubu"
+          placeholder="○○学部"
+          onChange={handleChange}
+        />
+        <br />
+
+        <label htmlFor="gakka">学科</label>
+        <br />
+        <input
+          type="text"
+          name="gakka"
+          id="gakka"
+          placeholder="学科"
+          onChange={handleChange}
+        />
+        <br />
+
+        <label htmlFor="phoneNum">電話番号</label>
+        <br />
+        <input
+          type="text"
+          name="phoneNum"
+          id="phoneNum"
+          placeholder="0x0xxxx xxxx"
+          onChange={handleChange}
+        />
+        <br />
+
         <label htmlFor="password">パスワード</label>
         <br />
         <input
