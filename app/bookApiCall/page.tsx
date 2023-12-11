d0c6cf92c31fefaea5b440221e806bd4dd39c432
BishwasKoirala <wasubisu69@gmail.com>
2023-12-11 14:01:30 +0900

    commit

--- app/bookApiCall/page.tsx
+++ app/bookApiCall/page.tsx
@@ -1,30 +1,50 @@
 'use client'
-import React, { useEffect } from 'react'
+import React, { useEffect , useState } from 'react'
 
+interface GoogleBookResponse {
+  items : Array<{
+    id : string ,
+    volumeInfo : {
+      title : string
+    }
+  }>
+}
 
 
 const page = () => {
 
+  const [bookData, setBookData] = useState<GoogleBookResponse['items'] | null>(null);
 
   useEffect(() => {
 
     const param : string = "9784534045843"
 
     const getData = async () => {
+
+      try{
       const query = await fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:${param}`)
+      const responsedata : GoogleBookResponse = await query.json()
 
-      const responsedata = await query.json()
+      if (query.ok) {
+        console.log('response from API' , responsedata)
+        setBookData(responsedata.items);
 
-      console.log('response from API' , responsedata)
 
+      } else {
+        throw new Error (`Error fetching data: ${query.status}`)
+      }
+      } catch (error) {
+        console.error('failed to fetch',error)
+      }
     }
 
     getData()
 
   },[])
 
-  
 
+  // console.log(setBookData)
+  // console.log(bookData)
 
   return (
     <h3>hello</h3>
