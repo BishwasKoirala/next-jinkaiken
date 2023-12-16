9489030c15e2423e01be3c19a03be37494e22f0d
BishwasKoirala <wasubisu69@gmail.com>
2023-12-16 21:15:13 +0900

    It seems ok

--- app/api/getBooks/route.tsx
+++ app/api/getBooks/route.tsx
@@ -1,18 +1,16 @@
 
-
-import  getFromIsbn  from "@/app/rentReturn/page"
-
-
-let theIsbn = '9784534045843'
-// let theIsbn = getFromIsbn() 
-
-export async function GET(request : Request) {
-  interface FilteredData {
+interface FilteredData {
     id : string , 
     title : string,
     isbn10 : string,
     isbn13 : string
   }
+
+let theIsbn = '9784534045843'
+// let gotData : any ; //get the processed object here
+
+export async function GET(request : Request) {
+  
 
   const query = await fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:${theIsbn}`)
 
@@ -25,6 +23,9 @@ export async function GET(request : Request) {
         isbn13 : holdQuery.items[0].volumeInfo.industryIdentifiers[1].identifier
   } 
 
+  console.log(filterData)
   return Response.json(filterData)
 
 }
+
+
