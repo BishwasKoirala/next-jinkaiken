8707aa9b8a008cb377239f1f512ea3c7826d15e6
BishwasKoirala <wasubisu69@gmail.com>
2023-12-19 11:49:22 +0900

    need to get api , then post response to db

--- app/registerBookDB/page.tsx
+++ app/registerBookDB/page.tsx
@@ -1,14 +1,66 @@
-'use client'
-import React, { useEffect , useState } from 'react'
-import GetData from './GetData'
+"use client";
+import React, { useState, FormEvent } from "react";
 
-const page = () => {
-  
-  return (
-    <>
-    <GetData/>
-  </>
-  )
+interface FormData {
+  isbn13: string;
+
 }
 
-export default page
+const RentReturnForm = () => {
+  const [formdata, setFormData] = useState<FormData>({
+    isbn13: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    setFormData({ ...formdata, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    const response = await fetch("/api/rentReturn", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formdata),
+    });
+
+    const data = await response.json();
+    if (response.ok) {
+      console.log("registration Success !!!", data);
+    } else {
+      console.log("Failed registration !!!", data);
+    }
+  };
+  return (
+
+    
+    <>
+      <div className="p-4 text-xl">
+        <p>本をデータベース登録{"(開発中)"}</p>
+      </div>
+
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="isbn13">ISBN13コード</label>
+          <br />
+          <input
+            type="text"
+            name="isbn13"
+            id="isbn13"
+            placeholder=""
+            onChange={handleChange}
+          />
+          <br />
+          <br />
+          <button type="reset" className="btn bg-red-600 mr-5 text-black">
+            RESET
+          </button>
+          <button type="submit" className=" text-black btn bg-green-600">
+            ・登録・
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default RentReturnForm;
