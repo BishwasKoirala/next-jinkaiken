c57bbb6fdb910c69df5643c772ec0120cd65549f
Rajan Valencia <47655366+rajanvalencia@users.noreply.github.com>
2024-01-07 20:27:56 +0900

    fix: underDevelopmentAert design

--- app/components/underDevelopmentAlert.tsx
+++ app/components/underDevelopmentAlert.tsx
@@ -3,7 +3,10 @@ import { FaExclamationCircle } from "react-icons/fa";
 
 export const UnderDevelopmentAlert = () => {
   return (
-    <div role="alert" className="flex justify-center alert my-2 text-gray-600">
+    <div
+      role="alert"
+      className="flex justify-center alert my-2 text-gray-600 text-lg"
+    >
       <FaExclamationCircle size={26} />
       <span>開発中</span>
     </div>
