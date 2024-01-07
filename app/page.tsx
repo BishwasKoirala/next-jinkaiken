a8359812ac8866b9c83595645b9b679add257eeb
Rajan Valencia <47655366+rajanvalencia@users.noreply.github.com>
2024-01-07 23:09:33 +0900

    comment: home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,14 @@ import { useSession } from "next-auth/react";
 export default function Home() {
   const { status } = useSession();
 
+  /**
+   * Redirect to /member/borrow if authenticated
+   * Redirect to /api/auth/signin if unauthenticated
+   */
   useEffect(() => {
     if (status === "authenticated") {
       window.location.href = "/member/borrow";
     }
-
     if (status === "unauthenticated") {
       window.location.href = "/api/auth/signin";
     }
