1d8794c2979f601531c0675ddb00b6d6939127e3
BishwasKoirala <wasubisu69@gmail.com>
2024-01-11 14:27:22 +0900

    made api to fetch book from bookrecords, did some directory refactoring

---
+++ app/api/frontFetch/burrowFormBookList/route.tsx
@@ -0,0 +1,13 @@
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request : NextRequest) {
+  const books = await prisma.storeBooks.findMany({
+    select : {
+      id : true , 
+      title : true
+    }
+  })
+
+  return NextResponse.json(books)
+}
--- app/member/register/page.tsx
+++ app/member/register/page.tsx
@@ -22,7 +22,7 @@ export default function Page() {
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
-    const response = await fetch("/api/register", {
+    const response = await fetch("/api/registerMember", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
