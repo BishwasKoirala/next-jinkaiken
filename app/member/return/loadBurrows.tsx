e87e4ce44d18815825cf7f6638dde8f814ca1dcb
BishwasKoirala <wasubisu69@gmail.com>
2024-01-17 15:19:29 +0900

    to make burrow page , load burrowed books of the user and return it with a button , next make the LoadBurrows compponent

---
+++ app/member/return/loadBurrows.tsx
@@ -0,0 +1,6 @@
+"use client"
+import { useState , useEffect } from "react"
+
+export const LoadBurrows = () => {
+
+}
--- app/member/return/page.tsx
+++ app/member/return/page.tsx
@@ -1,9 +1,19 @@
 "use client";
 
 import { UnderDevelopmentAlert } from "@/app/components/underDevelopmentAlert";
+// get the rented books of the student from StudentId
 
+// load the books on the screen
+
+// press button and do the returnwork
+
+interface FormData{
+  studentID : string;
+  bookId : string
+}
 const Return = () => {
   return <UnderDevelopmentAlert />;
+
 };
 
 export default Return;
