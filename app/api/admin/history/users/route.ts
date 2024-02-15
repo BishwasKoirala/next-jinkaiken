8b963186b7625344acda720a10151ee470029658
BishwasKoirala <wasubisu69@gmail.com>
2024-02-15 22:07:49 +0900

    made api to get users registered histry in recent order

---
+++ app/api/admin/history/users/route.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const response = await prisma?.user.findMany({
+    select: {
+      studentId: true,
+      name: true,
+      email: true,
+      gakubu: true,
+      gakka: true,
+      phoneNum: true,
+      isAdmin: true,
+      registered_at: true,
+    },
+    orderBy: {
+      registered_at: "desc",
+    },
+  });
+
+  if (!response) {
+    return NextResponse.json({ error: "response not found" }, { status: 404 });
+  }
+
+  return NextResponse.json(response);
+}
