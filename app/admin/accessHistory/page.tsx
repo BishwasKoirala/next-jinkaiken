6cae2d99db366c54cff692f87b24473c4442885a
BishwasKoirala <wasubisu69@gmail.com>
2024-02-16 14:54:07 +0900

    next do admin histrry fetch

---
+++ app/admin/accessHistory/page.tsx
@@ -0,0 +1,12 @@
+import { UnderDevelopmentAlert } from "@/app/components/underDevelopmentAlert";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <UnderDevelopmentAlert />
+    </div>
+  );
+};
+
+export default page;
