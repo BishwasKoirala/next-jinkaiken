d2752549e298f3b7d6ee47a062326b7c6be33747
BishwasKoirala <wasubisu69@gmail.com>
2024-02-17 02:35:19 +0900

    rebased dir

---
+++ app/admin/accessHistory/BookRegisters.tsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+
+const BookRegisters = () => {
+  const [registeredBooks, setRegisteredBooks] = useState();
+  return <div>BookRegisters</div>;
+};
+
+export default BookRegisters;
