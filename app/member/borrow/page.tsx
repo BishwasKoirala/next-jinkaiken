56201eb392f672be35ddb1c7dba630d88494258b
BishwasKoirala <wasubisu69@gmail.com>
2024-02-18 16:16:04 +0900

    use react hook forms and zod and zod validation and zod resolver to do the form in burrow form

--- app/member/borrow/page.tsx
+++ app/member/borrow/page.tsx
@@ -3,44 +3,58 @@
 import React, { useState, FormEvent } from "react";
 import { UnderDevelopmentAlert } from "@/app/components/underDevelopmentAlert";
 import { LoadBooks } from "./loadBooks";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-interface FormData {
-  studentId: string;
-  bookId: string;
-}
+
+const schema = z.object({
+  studentId: z
+    .string()
+    .min(9)
+    .max(9, { message: "student ID must be 9 digits" }),
+  bookId: z.string(),
+});
+
+type FormData = z.infer<typeof schema>;
 
 const RentReturnForm = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<FormData>({resolver : zodResolver(schema) })
+
   const [formdata, setFormData] = useState<FormData>({
     studentId: "",
     bookId: "",
   });
 
-// loads for select options booknames
-const books = LoadBooks();
+  // loads for select options booknames
+  const books = LoadBooks();
 
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
-  ) => {
-    setFormData({ ...formdata, [e.target.name]: e.target.value });
-  };
+  // const handleChange = (
+  //   e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  // ) => {
+  //   setFormData({ ...formdata, [e.target.name]: e.target.value });
+  // };
 
-  const handleSubmit = async (e: FormEvent) => {
-    e.preventDefault();
-    const response = await fetch("/api/bookTransaction/burrow/", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(formdata),
-    });
+  // const handleSubmit = async (e: FormEvent) => {
+  //   e.preventDefault();
+  //   const response = await fetch("/api/bookTransaction/burrow/", {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify(formdata),
+  //   });
 
-    const data = await response.json()
-    if (response.ok) {
-      console.log("registration Success !!!", data);
-    } else {
-      console.log("Failed registration !!!", data);
-    }
-  };
-
-
+  //   const data = await response.json();
+  //   if (response.ok) {
+  //     console.log("registration Success !!!", data);
+  //   } else {
+  //     console.log("Failed registration !!!", data);
+  //   }
+  // };
 
   // load options to render as select book to rent
   const options: JSX.Element[] = [];
@@ -59,22 +73,24 @@ const books = LoadBooks();
         <div className="py-2">
           <label htmlFor="studentId">学籍番号</label>
           <input
+          {...register('studentId')}
             id="studentId"
             name="studentId"
             type="text"
             className="input input-bordered w-full max-w-xs"
-            onChange={handleChange}
+            
           />
         </div>
         <div className="py-2">
           <label htmlFor="bookName">本のタイトル</label>
           <br />
           <select
+          {...register("bookId")}
             name="bookId"
             id="BookId"
             value={formdata.bookId}
             className="select select-bordered w-full max-w-xs align-middle"
-            onChange={handleChange}
+            
           >
             <option>本を選択</option>
             {options}
