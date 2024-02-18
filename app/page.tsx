3f4592d8b66a5aff49a69b4e60c01643fe3303bc
BishwasKoirala <wasubisu69@gmail.com>
2024-02-18 15:34:47 +0900

    edited home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,18 @@
-"use client";
-
-import { useEffect } from "react";
-import { useSession } from "next-auth/react";
-
-export default function Home() {
-  const { status } = useSession();
-
-  /**
-   * Redirect to /member/borrow if authenticated
-   * Redirect to /api/auth/signin if unauthenticated
-   */
-
-    useEffect(() => {
-      if (status === "authenticated") {
-        window.location.href = "/member/borrow";
-      }
-      if (status === "unauthenticated") {
-        window.location.href = "/welcome";
-      }
-    }, [status]);
-  
-    return <main className="text-center"></main>;
-  
+import React from 'react'
+import Link from 'next/link'
+import { UnderDevelopmentAlert } from './components/underDevelopmentAlert'
+const WelcomePage = () => {
+  return (
+    <div className='grid place-items-center pb-16 text-gray-500 text-lg'>
+      <UnderDevelopmentAlert />
+      <h1>
+        神奈川大学会計学研究部
+      </h1>
+      <Link href="/member/borrow" className='btn btn-wide mb-7 bg-green-600'>Login なしで続ける</Link>
+      <Link href="/api/auth/signin" className='btn btn-wide mb-7 bg-sky-300'>Log In</Link>
+      <Link href="/member/register" className='btn btn-wide bg-green-300'>入部登録</Link>
+    </div>
+  )
 }
+
+export default WelcomePage
