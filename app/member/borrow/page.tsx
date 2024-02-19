f8eccf72582d11cf344d7370591787771ea62f08
BishwasKoirala <wasubisu69@gmail.com>
2024-02-19 15:29:29 +0900

    did zod validation in burrow , next , do it in return

--- app/member/borrow/page.tsx
+++ app/member/borrow/page.tsx
@@ -4,16 +4,15 @@ import React, { useState, FormEvent } from "react";
 import { UnderDevelopmentAlert } from "@/app/components/underDevelopmentAlert";
 import { LoadBooks } from "./loadBooks";
 import { z } from "zod";
-import { useForm } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-
 
 const schema = z.object({
   studentId: z
     .string()
     .min(9)
     .max(9, { message: "student ID must be 9 digits" }),
-  bookId: z.string(),
+  bookId: z.string().min(1, {message : "select a book"}),
 });
 
 type FormData = z.infer<typeof schema>;
@@ -26,11 +25,6 @@ const RentReturnForm = () => {
     formState: { errors },
   } = useForm<FormData>({resolver : zodResolver(schema) })
 
-  const [formdata, setFormData] = useState<FormData>({
-    studentId: "",
-    bookId: "",
-  });
-
   // loads for select options booknames
   const books = LoadBooks();
 
@@ -40,21 +34,20 @@ const RentReturnForm = () => {
   //   setFormData({ ...formdata, [e.target.name]: e.target.value });
   // };
 
-  // const handleSubmit = async (e: FormEvent) => {
-  //   e.preventDefault();
-  //   const response = await fetch("/api/bookTransaction/burrow/", {
-  //     method: "POST",
-  //     headers: { "Content-Type": "application/json" },
-  //     body: JSON.stringify(formdata),
-  //   });
+  const onSubmit = async (formData: FieldValues) => {
+    const response = await fetch("/api/bookTransaction/burrow/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formData),
+    });
 
-  //   const data = await response.json();
-  //   if (response.ok) {
-  //     console.log("registration Success !!!", data);
-  //   } else {
-  //     console.log("Failed registration !!!", data);
-  //   }
-  // };
+    const data = await response.json();
+    if (response.ok) {
+      console.log("registration Success !!!", data);
+    } else {
+      console.log("Failed registration !!!", data);
+    }
+  };
 
   // load options to render as select book to rent
   const options: JSX.Element[] = [];
@@ -69,32 +62,35 @@ const RentReturnForm = () => {
   return (
     <div className="grid place-items-center pb-16 text-gray-500 text-lg">
       <UnderDevelopmentAlert />
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="py-2">
           <label htmlFor="studentId">学籍番号</label>
           <input
-          {...register('studentId')}
+            {...register("studentId")}
             id="studentId"
             name="studentId"
             type="text"
             className="input input-bordered w-full max-w-xs"
-            
           />
+          {errors.studentId && (
+            <p className="text-red-600">{errors.studentId.message}</p>
+          )}
         </div>
         <div className="py-2">
           <label htmlFor="bookName">本のタイトル</label>
           <br />
           <select
-          {...register("bookId")}
+            {...register("bookId")}
             name="bookId"
             id="BookId"
-            value={formdata.bookId}
             className="select select-bordered w-full max-w-xs align-middle"
-            
           >
-            <option>本を選択</option>
+            <option value="">本を選択</option>
             {options}
           </select>
+          {errors.bookId && (
+            <p className="text-red-500">{errors.bookId.message}</p>
+          )}
         </div>
         <div className="py-4 grid grid-cols-2 gap-2">
           <button type="reset" className="btn">
