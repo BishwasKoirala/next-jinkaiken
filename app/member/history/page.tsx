75a222c0315b1e0fc44e69c4fa4647e49b01c633
BishwasKoirala <wasubisu69@gmail.com>
2024-02-19 23:06:57 +0900

    added validation on all rent , return and history page

--- app/member/history/page.tsx
+++ app/member/history/page.tsx
@@ -2,30 +2,46 @@
 import React, { FormEvent, useRef, useState } from "react";
 import GetHistory from "./getHistory";
 import { UnderDevelopmentAlert } from "@/app/components/underDevelopmentAlert";
+import { z } from "zod";
+import { FieldValues, FormState, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  studentId : z.string().min(9,{message : "mest be 9 digits"}).max(9 , {message : "must be 9 digits"})
+})
+
+type FormData = z.infer<typeof schema>
 
 const History = () => {
+
+  const {register , handleSubmit , formState : {errors}} = useForm<FormData>({
+    resolver : zodResolver(schema)
+  })
+
+
+
   const studentIdRef = useRef<HTMLInputElement>(null);
   const [studentIdInput, setStudentIdInput] = useState<string>("");
   const [loadHistory, setLoadHistory] = useState<boolean>(false);
 
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault();
-    const gotStudentId = studentIdRef.current?.value;
-    if (gotStudentId) {
-      setStudentIdInput(gotStudentId);
-    }
+  const onSubmit = (FormData: FieldValues) => {
+    
+      setStudentIdInput(FormData.studentId);
+      setLoadHistory(true)
+    
   };
 
   return (
     <div className="grid place-items-center pb-16 text-gray-500 text-lg">
       <UnderDevelopmentAlert />
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="studenId" className="label-text text-xl">
           studentId
         </label>
         <br />
+        {errors.studentId && ( <p className="text-red-500">{errors.studentId.message}</p> )}
         <input
-          ref={studentIdRef}
+          {...register('studentId')}
           className="input input-bordered w-full max-w-xs"
           type="text"
         />
