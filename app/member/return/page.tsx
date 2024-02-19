7701094926be071959072130b7eabe26b0c770a0
BishwasKoirala <wasubisu69@gmail.com>
2024-02-19 16:09:09 +0900

    did form validation in return form

--- app/member/return/page.tsx
+++ app/member/return/page.tsx
@@ -2,42 +2,54 @@
 import { UnderDevelopmentAlert } from "@/app/components/underDevelopmentAlert";
 import LoadBurrows from "./loadBurrows";
 import { FormEvent, useRef, useState } from "react";
+import { string, z } from "zod";
+import { FieldValues, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 // get the rented books of the student from StudentId
 
 // load the books on the screen
 
 // press button and do the returnwork
 
-const Return = () => {
-  const studentIdRef = useRef<HTMLInputElement>(null);
+const schema = z.object({
+  studentId: z.string()
+    .min(9, { message: "student ID must be 9 digits" })
+    .max(9, { message: "studentId must be 9 digits" }),
+});
 
-  const [studentId, setStudentId] = useState<string>("");
+type FormData = z.infer<typeof schema>;
+
+const Return = () => {
+  const { register, handleSubmit, formState : {errors} } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
 
   const [loadBooks, setLoadBooks] = useState<boolean>(false);
+  const [studentId , setStudentId] = useState<string>('')
 
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault();
-    const gotStudentId = studentIdRef.current?.value;
-    if (gotStudentId) {
-      setStudentId(gotStudentId);
+  const onSubmit = (formData: FieldValues) => {
+      setStudentId(formData.studentId) 
       setLoadBooks(true);
     }
-  };
+  
 
   return (
     <div className="grid place-items-center pb-16 text-gray-500 text-lg">
       <UnderDevelopmentAlert />
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="studenId" className="label-text text-xl">
           studentId
         </label>
         <br />
+        {errors.studentId && (
+          <p className="text-red-500">{errors.studentId.message}</p>
+        )}
         <input
-          ref={studentIdRef}
+          {...register('studentId')}
           className="input input-bordered w-full max-w-xs"
           type="text"
         />
-        <br />
+        
         <button type="submit" className="btn bg-blue-400 m-3">
           Load
         </button>
