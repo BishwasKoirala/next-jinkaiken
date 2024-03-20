dd5236e6808026d034170b5b6a5dcb35a6cbc299
BishwasKoirala <wasubisu69@gmail.com>
2024-03-20 13:16:21 +0900

    fixed the production bug of not refetching available bookd
    it was a prisma bug , need to      [200~export const dynamic = 'force-dynamic'

--- app/api/v2/books/available/route.ts
+++ app/api/v2/books/available/route.ts
@@ -1,8 +1,11 @@
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
+// it fix the not refetching available books in borrowbooks
+export const dynamic = 'force-dynamic'
+
 // fetches all the book that are rentable
-export async function GET(request : NextRequest) {
+export async function GET(request : NextRequest , response : NextResponse) {
   const books = await prisma.storeBooks.findMany({
     where : {rentable : true},
     select : {
